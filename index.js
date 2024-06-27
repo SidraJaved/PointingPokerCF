d8a2c92906046c9621e18e3178213d2b1c4ade79
SidraJaved <sidrajaved92@gmail.com>
2024-06-27 11:09:26 +0100

    testing git integration with CF

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const functions = require('@google-cloud/functions-framework');
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
 functions.http('homeGet', (req, res) => {
+    console.log("hello");
     res.sendFile(join(__dirname, 'index.html'));
 });
