21c29b9af5bf64ad9b236ec33f2e2e1db2ae95c1
SidraJaved <sidrajaved92@gmail.com>
2024-06-27 11:31:53 +0100

    fixing home page

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const functions = require('@google-cloud/functions-framework');
+const path = require('path');
 
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
-functions.http('homeGet', (req, res) => {
+functions.http('home', (req, res) => {
     console.log("hello");
-    res.sendFile(join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
